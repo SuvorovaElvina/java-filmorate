9c7a8e320137cc7cab3f4b5b3a876f3f9b7705f0
Elvina <elvina.smirnova.205@mail.ru>
2023-02-20 18:50:45 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -18,6 +18,7 @@ public class Film {
     private LocalDate releaseDate;
     @Positive(message = "Продолжительность фильма должна быть положительной.")
     private Long duration;
+
     public Film(Integer id, String name, String description, LocalDate releaseDate, Long duration) {
         this.id = id;
         this.name = name;
