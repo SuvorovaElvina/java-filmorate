9391169905a1e9f78f3d47d64b20a1ef548e6851
Elvina <elvina.smirnova.205@mail.ru>
2023-02-20 18:51:17 +0300

    feat: проверка на пробел в логине и конструктор

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
@@ -14,8 +15,17 @@ public class User {
     @Email(message = "Введён не email.")
     private String email;
     @NotBlank(message = "Логин не должно быть пустым.")
+    @Pattern(regexp = "^[A-Za-z0-9]+$")
     private String login;
     private String name;
     @Past(message = "День рождения не должно быть в будущем.")
     private LocalDate birthday;
+
+    public User(Integer id, String email, String login, String name, LocalDate birthday) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
