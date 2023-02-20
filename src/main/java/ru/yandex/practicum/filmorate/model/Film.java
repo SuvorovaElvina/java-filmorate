055a43e54d4300e92fd3a9bde99a0f10f4464bbd
Elvina <elvina.smirnova.205@mail.ru>
2023-02-20 14:22:44 +0300

    feat: проверка на null

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
@@ -11,7 +12,9 @@ public class Film {
     private Integer id;
     @NotBlank(message = "Имя не должно быть пустым.")
     private String name;
+    @NotNull(message = "Описание должно быть заполнено.")
     private String description;
+    @NotNull(message = "Дата релиза должна быть заполнена.")
     private LocalDate releaseDate;
     @Positive(message = "Продолжительность фильма должна быть положительной.")
     private Long duration;
