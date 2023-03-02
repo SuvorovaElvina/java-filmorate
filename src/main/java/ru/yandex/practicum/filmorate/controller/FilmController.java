e6b63b8b017eb97bbc1441e9061514a5a0e45554
Elvina <elvina.smirnova.205@mail.ru>
2023-03-02 17:38:02 +0300

    feat: добавила зависимость и убрала бизнес-логику

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,56 +3,73 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.throwable.ValidationException;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private final Map<Integer, Film> films = new HashMap<>();
     private static final LocalDate AFTER_RELEASE_DATE = LocalDate.of(1895, Month.DECEMBER, 28);
-    private Integer id = 1;
+    private final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
 
     @GetMapping
-    public ArrayList<Film> getAllFilms() {
-        log.debug("Количество фильмов - {}", films.size());
-        return new ArrayList<>(films.values());
+    public List<Film> getAllFilms() {
+        log.debug("Количество фильмов - {}", filmService.getFilms().size());
+        return new ArrayList<>(filmService.getFilms().values());
     }
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
-        if (!films.containsKey(film.getId())) {
-            film.setId(id++);
-            validateFilm(film);
-            films.put(film.getId(), film);
-        } else {
-            log.error("Этот фильм уже создан.");
-            throw new ValidationException("Этот фильм уже создан.");
-        }
+        validateFilm(film);
+        filmService.createFilm(film);
         return film;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
-        if (films.containsKey(film.getId())) {
-            validateFilm(film);
-            films.put(film.getId(), film);
-        } else {
-            log.error("Данного фильма нет в списках.");
-            throw new ValidationException("Данного фильма нет в списках.");
-        }
+        validateFilm(film);
+        filmService.updateFilm(film);
         return film;
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilms(@PathVariable int id) {
+        return filmService.getFilm(id);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularFilms(@RequestParam(required = false) String count) {
+        if (count == null) {
+            return filmService.getPopularFilms(10);
+        } else {
+            return filmService.getPopularFilms(Integer.parseInt(count));
+        }
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable("id") int id, @PathVariable("userId") int userId) {
+        filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(@PathVariable("id") int id, @PathVariable("userId") int userId) {
+        filmService.removeLike(id, userId);
     }
 
     private void validateFilm(Film film) {
         if (film.getReleaseDate().isBefore(AFTER_RELEASE_DATE)) {
+            log.error("Не прошёл валидацию.");
             throw new ValidationException("Фильм должен быть не раньше " + AFTER_RELEASE_DATE.getDayOfMonth()
                     + " " + AFTER_RELEASE_DATE.getMonth() + " " + AFTER_RELEASE_DATE.getYear());
         }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,49 +1,69 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.throwable.ValidationException;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<>();
-    private Integer id = 1;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
-    public ArrayList<User> users() {
-        log.debug("Всего пользователей: {}", users.size());
-        return new ArrayList<>(users.values());
+    public List<User> users() {
+        log.debug("Всего пользователей: {}", userService.getUsers().size());
+        return new ArrayList<>(userService.getUsers().values());
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        if (!users.containsKey(user.getId())) {
-            user.setId(id++);
-            validateUser(user);
-        } else {
-            log.error("Этот пользователь уже зарегистрирован.");
-            throw new ValidationException("Этот пользователь уже зарегистрирован.");
-        }
+        validateUser(user);
+        userService.createUser(user);
         return user;
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
-        if (users.containsKey(user.getId())) {
-            validateUser(user);
-        } else {
-            log.error("Такого пользователя нет в списке зарегистрированых.");
-            throw new ValidationException("Такого пользователя нет в списке зарегистрированых.");
-        }
+        validateUser(user);
+        userService.updateUser(user);
         return user;
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable int id) {
+        return userService.getUser(id);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        userService.addFriend(id, friendId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getFriends(@PathVariable int id) {
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+        return userService.getCommonFriends(id, otherId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void removeFriend(@PathVariable int id, @PathVariable int friendId) {
+        userService.removeFriend(id, friendId);
     }
 
     private void validateUser(User user) {
@@ -51,6 +71,5 @@ public class UserController {
             user.setName(user.getLogin());
         }
         log.debug(user.toString());
-        users.put(user.getId(), user);
     }
 }
