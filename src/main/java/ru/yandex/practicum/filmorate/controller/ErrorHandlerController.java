d9e0ef9c90c483d549c94c6833b960e4f08c5d32
Elvina <elvina.smirnova.205@mail.ru>
2023-03-06 14:23:15 +0300

    feat: добавила проверку исключениям

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandlerController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandlerController.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.throwable.ConflictException;
 import ru.yandex.practicum.filmorate.throwable.IncorrectCountException;
+import ru.yandex.practicum.filmorate.throwable.NotFoundException;
 import ru.yandex.practicum.filmorate.throwable.ValidationException;
 
 import java.util.Map;
@@ -22,6 +24,24 @@ public class ErrorHandlerController {
     public Map<String, String> handleIncorrectCountException(final IncorrectCountException e) {
         return Map.of(
                 "error", "Ошибка с параметром count.",
+                "errorMessage", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(final NotFoundException e) {
+        return Map.of(
+                "error", "Ошибка - не найдено.",
+                "errorMessage", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleConflictException(final ConflictException e) {
+        return Map.of(
+                "error", "Произошла ошибка!",
                 "errorMessage", e.getMessage()
         );
     }
