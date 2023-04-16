601fe5da469b896c8459ff6c9c738ae7304c3f49
vitox79 <viktor.monarev@gmail.com>
2023-04-16 19:53:17 +0700

    add recommendation4

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -40,8 +40,8 @@ public class Film {
         this.mpa = mpa;
         this.genres = genres;
     }
-    private List<Director> directors = new ArrayList<>();
 
+    private List<Director> directors = new ArrayList<>();
 
     public Film(Integer id, String name, String description, LocalDate releaseDate, Long duration, Mpa mpa) {
         this.id = id;
